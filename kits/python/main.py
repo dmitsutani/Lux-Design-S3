9246445c08bcdac6dac2edcd907937ce7d4044e9
dmitsutani <dani.mitsu@gmail.com>
2024-12-13 15:53:19 -0300

    initial commit

--- kits/python/main.py
+++ kits/python/main.py
@@ -27,6 +27,7 @@ def agent_fn(observation, configurations):
     agent = agent_dict[player]
     actions = agent.act(step, from_json(obs), remainingOverageTime)
     return dict(action=actions.tolist())
+
 if __name__ == "__main__":
     
     def read_input():
